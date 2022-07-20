9a5642aa59e747fd9c6f0ded904914ac8af13ed0
Janet <Janet_Lam@live.ca>
2022-07-20 16:46:51 -0400

    Finding the middle of an array

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`)
+  }
+};
+
+const middle = (notMiddle) => {
+  let total = notMiddle.length
+  let middle = total / 2
+  if (total <= 2) {
+    return []
+  }
+  if (!Number.isInteger(middle)) {
+    return [Math.floor(middle) + 1]
+  }
+  else {
+    return [Math.floor(middle), Math.floor(middle) + 1]
+  }
+}
+
+assertArraysEqual(middle([1]), []) // => []
+assertArraysEqual(middle([1, 2]), []) // => []
+assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+
