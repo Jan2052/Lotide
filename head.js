fe31171805a66563ff4da1ce3aa1b8b892ff0e3c
Janet <Janet_Lam@live.ca>
2022-08-06 19:54:07 -0400

    Refactored

--- head.js
+++ head.js
@@ -1,17 +1,23 @@
+// const assertEqual = require('../assertEqual');
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    return "true";
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
   }
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`)
-  return "false";
 };
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 1);
 
+
+// Returns first item in the array
 const head = function (array) {
   console.log(array[0])
-  return array
+  return array[0]
 }
-
-
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// module.exports = head;
