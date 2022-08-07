5f7fc2baed127b008d25279c1fc4448ba2a4a662
Janet <Janet_Lam@live.ca>
2022-08-07 00:50:55 -0400

    Used shorthand

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
+module.exports = { head, tail, middle };
